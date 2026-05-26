8de9713aa01d18ad4b56c1f1e44f1fa17713b5de
ERMIASJOHNY <johnyjon1221@gmail.com>
2026-05-26 14:20:30 +0300

    Refactor PaymentCategory and Transaction classes

--- lib/models/transaction.g.dart
+++ lib/models/transaction.g.dart
@@ -1,154 +1,150 @@
-import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart' hide Transaction;
-import 'package:hive/hive.dart';
+// GENERATED CODE - DO NOT MODIFY BY HAND
 
-part 'transaction.g.dart';
+part of 'transaction.dart';
 
-@HiveType(typeId: 2)
-enum TransactionType { 
-  @HiveField(0)
-  income, 
-  @HiveField(1)
-  expense 
-}
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
 
-@HiveType(typeId: 1)
-class PaymentCategory {
-  @HiveField(0)
-  final String id;
-  @HiveField(1)
-  final String name;
-  @HiveField(2)
-  final int iconCode;
-  @HiveField(3)
-  final int colorValue;
+class PaymentCategoryAdapter extends TypeAdapter<PaymentCategory> {
+  @override
+  final int typeId = 1;
 
-  IconData get icon => IconData(iconCode, fontFamily: 'MaterialIcons');
-  Color get color => Color(colorValue);
+  @override
+  PaymentCategory read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return PaymentCategory(
+      id: fields[0] as String,
+      name: fields[1] as String,
+      iconCode: fields[2] as int,
+      colorValue: fields[3] as int,
+    );
+  }
 
-  const PaymentCategory({
-    required this.id,
-    required this.name,
-    required this.iconCode,
-    required this.colorValue,
-  });
+  @override
+  void write(BinaryWriter writer, PaymentCategory obj) {
+    writer
+      ..writeByte(4)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.name)
+      ..writeByte(2)
+      ..write(obj.iconCode)
+      ..writeByte(3)
+      ..write(obj.colorValue);
+  }
 
-  // Keep these for UI convenience if needed
-  static PaymentCategory create({
-    required String id,
-    required String name,
-    required IconData icon,
-    required Color color,
-  }) => PaymentCategory(
-    id: id,
-    name: name,
-    iconCode: icon.codePoint,
-    colorValue: color.toARGB32(),
-  );
-
-  Map<String, dynamic> toMap() => {
-        'id': id,
-        'name': name,
-        'icon': iconCode,
-        'color': colorValue,
-      };
-
-  factory PaymentCategory.fromMap(Map<String, dynamic> map) => PaymentCategory(
-        id: map['id'],
-        name: map['name'],
-        iconCode: map['icon'],
-        colorValue: map['color'],
-      );
+  @override
+  int get hashCode => typeId.hashCode;
 
   @override
   bool operator ==(Object other) =>
       identical(this, other) ||
-      other is PaymentCategory &&
+      other is PaymentCategoryAdapter &&
           runtimeType == other.runtimeType &&
-          id == other.id;
+          typeId == other.typeId;
+}
+
+class TransactionAdapter extends TypeAdapter<Transaction> {
+  @override
+  final int typeId = 0;
 
   @override
-  int get hashCode => id.hashCode;
-}
-
-@HiveType(typeId: 0)
-class Transaction {
-  @HiveField(0)
-  final String id;
-  @HiveField(1)
-  final String title;
-  @HiveField(2)
-  final String? subtitle;
-  @HiveField(3)
-  final double amount;
-  @HiveField(4)
-  final TransactionType type;
-  @HiveField(5)
-  final String categoryId; // Store ID to link with provider
-  @HiveField(6)
-  final DateTime date;
-  @HiveField(7)
-  final String? avatarInitials;
-  @HiveField(8)
-  final int? avatarColorValue;
-  @HiveField(9)
-  final DateTime updatedAt;
-
-  Transaction({
-    required this.id,
-    required this.title,
-    this.subtitle,
-    required this.amount,
-    required this.type,
-    required this.categoryId,
-    required this.date,
-    this.avatarInitials,
-    this.avatarColorValue,
-    required this.updatedAt,
-  });
-
-  Map<String, dynamic> toMap() => {
-        'id': id,
-        'title': title,
-        'subtitle': subtitle,
-        'amount': amount,
-        'type': type.index,
-        'category': categoryId,
-        'date': Timestamp.fromDate(date),
-        'avatarInitials': avatarInitials,
-        'avatarColor': avatarColorValue,
-        'updatedAt': Timestamp.fromDate(updatedAt),
-      };
-
-  factory Transaction.fromMap(Map<String, dynamic> map) {
-    DateTime parsedDate;
-    if (map['date'] is Timestamp) {
-      parsedDate = (map['date'] as Timestamp).toDate();
-    } else {
-      parsedDate = DateTime.parse(map['date']);
-    }
-
-    DateTime parsedUpdatedAt;
-    if (map['updatedAt'] is Timestamp) {
-      parsedUpdatedAt = (map['updatedAt'] as Timestamp).toDate();
-    } else if (map['updatedAt'] != null) {
-      parsedUpdatedAt = DateTime.parse(map['updatedAt']);
-    } else {
-      parsedUpdatedAt = parsedDate; // Fallback
-    }
-
+  Transaction read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
     return Transaction(
-      id: map['id'],
-      title: map['title'],
-      subtitle: map['subtitle'],
-      amount: map['amount'],
-      type: TransactionType.values[map['type']],
-      categoryId: map['category'],
-      date: parsedDate,
-      avatarInitials: map['avatarInitials'],
-      avatarColorValue: map['avatarColor'],
-      updatedAt: parsedUpdatedAt,
+      id: fields[0] as String,
+      title: fields[1] as String,
+      subtitle: fields[2] as String?,
+      amount: fields[3] as double,
+      type: fields[4] as TransactionType,
+      categoryId: fields[5] as String,
+      date: fields[6] as DateTime,
+      avatarInitials: fields[7] as String?,
+      avatarColorValue: fields[8] as int?,
+      updatedAt: fields[9] as DateTime,
     );
   }
+
+  @override
+  void write(BinaryWriter writer, Transaction obj) {
+    writer
+      ..writeByte(10)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.title)
+      ..writeByte(2)
+      ..write(obj.subtitle)
+      ..writeByte(3)
+      ..write(obj.amount)
+      ..writeByte(4)
+      ..write(obj.type)
+      ..writeByte(5)
+      ..write(obj.categoryId)
+      ..writeByte(6)
+      ..write(obj.date)
+      ..writeByte(7)
+      ..write(obj.avatarInitials)
+      ..writeByte(8)
+      ..write(obj.avatarColorValue)
+      ..writeByte(9)
+      ..write(obj.updatedAt);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is TransactionAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
 }
 
+class TransactionTypeAdapter extends TypeAdapter<TransactionType> {
+  @override
+  final int typeId = 2;
+
+  @override
+  TransactionType read(BinaryReader reader) {
+    switch (reader.readByte()) {
+      case 0:
+        return TransactionType.income;
+      case 1:
+        return TransactionType.expense;
+      default:
+        return TransactionType.income;
+    }
+  }
+
+  @override
+  void write(BinaryWriter writer, TransactionType obj) {
+    switch (obj) {
+      case TransactionType.income:
+        writer.writeByte(0);
+        break;
+      case TransactionType.expense:
+        writer.writeByte(1);
+        break;
+    }
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is TransactionTypeAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
